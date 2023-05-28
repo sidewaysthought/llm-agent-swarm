d775610a0cefd47c5967cbf156e5bd17c626615b
David Delgado <david@sidewaysthought.net>
2023-05-28 03:39:27 -0500

    Memory pruning

--- memory_manager/memory_manager.py
+++ memory_manager/memory_manager.py
@@ -4,12 +4,16 @@ from rdflib import Graph, URIRef, Literal, Namespace
 class MemoryManager:
 
     def __init__(self, sys_namespace:str):
+
+        # Constants
+        self.SEARCH_THRESHOLD = 50
         
         # I'm using two database styles because I'm greedy and want the best of both worlds where graph can
         # be used for triples and rapid subject searches, and the vector database can be used for detailed
         # semantic searches that the graph database can't do.
         self.graph = Graph()
         self.namespace = Namespace(sys_namespace)
+        self.usage_counter = {}
 
 
     def remember(self, message_obj:dict = {}):
@@ -30,6 +34,9 @@ class MemoryManager:
             self.graph.add((memory_uri, self.namespace.to_name, Literal(message_obj["to"])))
             self.graph.add((memory_uri, self.namespace.datetime, Literal(message_obj["datetime"])))
             self.graph.add((memory_uri, self.namespace.message, Literal(message_obj["message"])))
+            self.usage_counter[memory_id] = 0
+
+        self.prune()
     
 
     def recall(self, search_term:str = '') -> list:
@@ -46,12 +53,16 @@ class MemoryManager:
         results = []
 
         for memory_uri in self.graph.subjects(predicate=self.namespace.message, object=Literal(search_term)):
+            memory_id = str(memory_uri).split("/")[-1]
+            self.usage_counter[memory_id] += 1
             memory = {}
             memory["from"] = str(self.graph.value(subject=memory_uri, predicate=self.namespace.from_name))
             memory["to"] = str(self.graph.value(subject=memory_uri, predicate=self.namespace.to_name))
             memory["datetime"] = str(self.graph.value(subject=memory_uri, predicate=self.namespace.datetime))
             memory["message"] = str(self.graph.value(subject=memory_uri, predicate=self.namespace.message))
             results.append(memory)
+
+        self.prune()
 
         return results
     
@@ -71,6 +82,8 @@ class MemoryManager:
 
         memories_to_remove = []
         for memory_uri in self.graph.subjects(predicate=self.namespace.message, object=Literal(search_term)):
+            memory_id = str(memory_uri).split("/")[-1]
+            del self.usage_counter[memory_id]
             memories_to_remove.append(memory_uri)
 
         if len(memories_to_remove) > 0:
@@ -88,4 +101,15 @@ class MemoryManager:
         Prune the memory.
         """
 
-        return True
+        memories_to_remove = []
+
+        for memory_id, usage_count in self.usage_counter.items():
+            if usage_count >= self.SEARCH_THRESHOLD:
+                memory_uri = URIRef(self.namespace + memory_id)
+                memories_to_remove.append(memory_uri)
+
+        for memory_uri in memories_to_remove:
+            self.graph.remove((memory_uri, None, None))
+            self.graph.remove((None, None, memory_uri))
+            memory_id = memory_uri.split("/")[-1]
+            del self.usage_counter[memory_id] 
