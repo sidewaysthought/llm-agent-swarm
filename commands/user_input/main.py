7cf1025e8e2d996703dab6456d58f6bca57c673b
David Delgado <david@sidewaysthought.net>
2023-06-15 00:00:52 -0500

    Complete the ask user command

--- commands/user_input/main.py
+++ commands/user_input/main.py
@@ -8,13 +8,25 @@ class UserInputCommand(Command):
 
     
     def call(self, asking_agent:str, arguments:dict) -> dict:
+        """
+        Ask the user for input and return the response.
+        
+        Args:
+            asking_agent (str): The name of the agent asking for input
+            arguments (dict): The arguments for the command
+            
+        Returns:
+            dict: The response from the command
+        """
 
         response = self.create_response(asking_agent)
         is_valid = self.validate_arguments(arguments)
 
         if is_valid:
-            # Your code here
-            pass
+            user_input = input(arguments['message'])
+            response['data'] = user_input
+            response['status'] = self.STATUS_SUCCESS
+            response['message'] = 'The user has responded with this message.'
         else:
             response['status'] = self.STATUS_ERROR
             response['message'] = 'Invalid arguments'
